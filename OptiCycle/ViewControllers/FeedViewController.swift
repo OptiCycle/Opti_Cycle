00b5353d82bab5d95c0cd1c34e1fe7e08b835c64
patelisii <34968106+patelisii@users.noreply.github.com>
2021-04-10 15:22:20 -0400

    feed view controller

--- OptiCycle/ViewControllers/FeedViewController.swift
+++ OptiCycle/ViewControllers/FeedViewController.swift
@@ -6,29 +6,64 @@
 //
 
 import UIKit
+import Parse
 
 // Add UITableViewDataSource
-class FeedViewController: UIViewController, UITableViewDelegate {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
 
     @IBOutlet weak var tableView: UITableView!
     
+//    var refreshControl: UIRefreshControl!
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-//        tableView.delegate = self
-//        tableView.dataSource = self
+        
+//        refreshControl = UIRefreshControl()
+//        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+//        tableView.refreshControl = refreshControl
+        
+        refreshData()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
+    @objc func refreshData(){
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 30
+        
+        query.findObjectsInBackground{ (posts, error) in
+            if posts != nil{
+                self.posts = posts!
+                self.posts.reverse()
+                self.tableView.reloadData()
+            }
+        }
+//        self.refreshControl.endRefreshing()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+//        let post = posts[indexPath.row]
+//        let user = post["author"] as! PFUser
+//        let name = user.username as! String
+//        let item = post["item"] as! String
+//        let conf = post["confidence"] as! String
+//        cell.postLabel.text = name + " recycled " + item + " with a confidence of " + conf
+//        let imageLabel = post["image_label"] as! String
+//        cell.postImage.image = UIImage(named: imageLabel)
+        return cell
+    }
 
     /*
     // MARK: - Navigation
