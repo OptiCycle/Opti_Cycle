38ce998ffc5a7ac7f9ba396d95395ec4f34b6434
Mikael Kaufman <mikael.j.kaufman@gmail.com>
2021-05-26 13:06:47 -0400

    NEW: Added badge assets and made a collection view in Profile Page for all badges for users to collect

---
+++ OptiCycle/Cells/BadgeCell.swift
@@ -0,0 +1,17 @@
+//
+//  BadgeCell.swift
+//  OptiCycle
+//
+//  Created by Mikael Joseph Kaufman on 5/26/21.
+//
+
+import UIKit
+
+class BadgeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var badgeImageView: UIImageView!
+    
+    //Marker Felt Wide 15.0, Centered, lines == 0
+    @IBOutlet weak var badgeLabel: UILabel!
+    
+}
--- OptiCycle/ViewControllers/ProfileViewController.swift
+++ OptiCycle/ViewControllers/ProfileViewController.swift
@@ -9,17 +9,27 @@ import UIKit
 import Parse
 import MBCircularProgressBar
 
-class ProfileViewController: UIViewController {
-
+class ProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var postsProgressBarView: MBCircularProgressBarView!
     @IBOutlet weak var treesProgressBarView: MBCircularProgressBarView!
     @IBOutlet weak var badgesProgressBarView: MBCircularProgressBarView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        layout.itemSize = CGSize(width: width, height: 180)
         
         self.postsProgressBarView.value = 0
         self.treesProgressBarView.value = 0
@@ -51,6 +61,18 @@ class ProfileViewController: UIViewController {
         {
             self.badgesProgressBarView.value = 0
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Badge", for: indexPath) as! BadgeCell
+        
+        cell.badgeImageView.image = UIImage(named: "allBadgesBadge")
+        
+        return cell
     }
 
     @IBAction func UnwindSettingsViewController(unwindSegue: UIStoryboardSegue){}
