0864fc2f6b8e85689d7adcd3f5398017e657feb2
maxskeen <69686704+maxskeen@users.noreply.github.com>
2021-06-02 12:43:34 -0400

    Added animation when you click on Tab Bar Items

---
+++ OptiCycle/AnimatedTabBarController.swift
@@ -0,0 +1,90 @@
+//
+//  AnimatedTabBarController.swift
+//  OptiCycle
+//
+//  Created by Max Skeen on 6/2/21.
+//
+
+import UIKit
+
+class AnimatedTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+}
+
+extension AnimatedTabBarController: UITabBarControllerDelegate {
+
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return MyTransition(viewControllers: tabBarController.viewControllers)
+    }
+}
+
+class MyTransition: NSObject, UIViewControllerAnimatedTransitioning {
+
+    let viewControllers: [UIViewController]?
+    let transitionDuration: Double = 0.5
+
+    init(viewControllers: [UIViewController]?) {
+        self.viewControllers = viewControllers
+    }
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return TimeInterval(transitionDuration)
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+
+        guard
+            let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from),
+            let fromView = fromVC.view,
+            let fromIndex = getIndex(forViewController: fromVC),
+            let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to),
+            let toView = toVC.view,
+            let toIndex = getIndex(forViewController: toVC)
+            else {
+                transitionContext.completeTransition(false)
+                return
+        }
+
+        let frame = transitionContext.initialFrame(for: fromVC)
+        var fromFrameEnd = frame
+        var toFrameStart = frame
+        fromFrameEnd.origin.x = toIndex > fromIndex ? frame.origin.x - frame.width : frame.origin.x + frame.width
+        toFrameStart.origin.x = toIndex > fromIndex ? frame.origin.x + frame.width : frame.origin.x - frame.width
+        toView.frame = toFrameStart
+
+        DispatchQueue.main.async {
+            transitionContext.containerView.addSubview(toView)
+            UIView.animate(withDuration: self.transitionDuration, animations: {
+                fromView.frame = fromFrameEnd
+                toView.frame = frame
+            }, completion: {success in
+                fromView.removeFromSuperview()
+                transitionContext.completeTransition(success)
+            })
+        }
+    }
+
+    func getIndex(forViewController vc: UIViewController) -> Int? {
+        guard let vcs = self.viewControllers else { return nil }
+        for (index, thisVC) in vcs.enumerated() {
+            if thisVC == vc { return index }
+        }
+        return nil
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
